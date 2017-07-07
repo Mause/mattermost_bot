1b40d6fee89879d322f4b59c94ea5fe2086b249b
Dominic May <me@mause.me>
2017-07-07 20:26:24 +0800

    Replace get_team_id() information source

--- mattermost_bot/dispatcher.py
+++ mattermost_bot/dispatcher.py
@@ -177,7 +177,7 @@ class Message(object):
         return channel_name
 
     def get_team_id(self):
-        return self._client.api.team_id
+        return self._body['data']['team_id']
 
     def get_message(self):
         return self._body['data']['post']['message'].strip()
